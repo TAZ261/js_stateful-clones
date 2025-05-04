6fbb7687fe2eb7c1c37d04474ffea2ec4f275c28
Roman <hpp1ovh@gmail.com>
2025-05-04 18:58:40 +0300

    Task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,29 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-}
+  let curentState = { ...state };
+  const result = [];
 
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      curentState = { ...curentState, ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      curentState = { ...curentState };
+
+      for (const key of action.keysToRemove) {
+        delete curentState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      curentState = {};
+    }
+
+    result.push({ ...curentState });
+  }
+
+  return result;
+}
 module.exports = transformStateWithClones;
